15b060f6260b449ab9e845e8bc2f16a2af86b596
Erand <=>
2021-10-12 17:25:42 +0200

    unit test

---
+++ UnitTestProjectWebApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectWebApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectWebApp")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb3ee817-0f9a-40e8-b092-8aebec0d786c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProjectWebApp/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace UnitTestProjectWebApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                WebApp.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
