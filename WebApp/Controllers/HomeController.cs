5dbc19437834c696579531e4f7922638b03e9c8c
Erand <=>
2021-10-14 17:22:24 +0200

    kot

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -28,9 +28,6 @@ namespace WebApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public void Kot()
-        {
-           
-        }
+        
     }
 }
